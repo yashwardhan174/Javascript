17437f647eba0119384d6b0a7c6e34b018e8fb5e
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2024-08-16 08:33:36 +0000

    variables in js

--- JS/variables.js
+++ JS/variables.js
@@ -1,1 +1,17 @@
+const acc_ID = 1234456  //const cant be changed 
+let acc_Email = "yash@gmail.com"
+var acc_pwd = "12345" //dont use var much 
+acc_City = "Indore"
+
+acc_Email = "yashwardhan@gmail.com"
+acc_pwd = "234232"
+acc_City = "Pune"
+
+/*Prefer not using Var 
+because of issues in Block scope and functional scope*/
+
+
+//console.log(accountID);
+
+console.table([acc_ID , acc_Email , acc_pwd , acc_City])
 
