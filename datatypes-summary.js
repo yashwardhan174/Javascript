767fac764c6edd23468beee488d035e796aca257
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2024-08-25 04:31:36 +0000

    summary

---
+++ datatypes-summary.js
@@ -0,0 +1,19 @@
+//Primitive
+
+// 7 Types: String , Null , Undefined , Symbol , number , BigInt , boolean
+
+//Reference or Non-Primitive
+
+// Array , Objects , Functions
+
+const supHeroes = ["Shaktiman" , "Ironman" , "Thor"] //This is an array
+
+let myObj{
+    name = "Yash",
+    age = "20",
+
+} //This is an Object
+
+const myFunciton = function{
+    console.log("Hello World");
+}
