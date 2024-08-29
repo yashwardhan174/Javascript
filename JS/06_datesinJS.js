6ba204da59e64c602b2a8909ed74a2a4ddd359d8
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2024-08-29 13:22:21 +0000

    dates in JS

---
+++ JS/06_datesinJS.js
@@ -0,0 +1,18 @@
+//DATES
+//Date is an Object in JS
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+
+let myCreatedDate = new Date(2024 , 6 , 1) //in JS months starts with 0
+// console.log(myCreatedDate.toDateString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
