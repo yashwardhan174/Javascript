be6c41599f83a8603cb08d42ff67f78ff55f07fa
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2024-08-30 15:25:58 +0000

    name changes

---
+++ JS Basics 2/01_arrays.js
@@ -0,0 +1,12 @@
+//Arrays
+
+const myArr = [0 , 1 , 2 , 3 , 4 , 5]
+const Superheros = ["Ironman" , "Thor"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1])
+
+//Array Methods
+
+myArr.push(6)
+console.log(myArr);
