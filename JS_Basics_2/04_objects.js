a20c1b875a5212c17f37ffd1920829ae0dd5fc38
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2024-09-10 14:30:59 +0000

    objects 2

---
+++ JS_Basics_2/04_objects.js
@@ -0,0 +1,53 @@
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "yashvardhannigam@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Yashwardhan",
+            lastname: "Nigam"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = Object.assign({}, obj1 , obj2)
+const obj3 = {...obj1 , ...obj2}
+// console.log(obj3);
+
+
+const user = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
+user[1].email
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
