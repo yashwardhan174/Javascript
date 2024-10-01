be78a04f9ef9bea45ea3b9d26f1668ba7f5993c2
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2024-10-01 08:12:16 +0000

    loops

---
+++ JS_Iterations_5/five.js
@@ -0,0 +1,42 @@
+const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+// coding.forEach( function (item){
+//     console.log(item);
+    
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+function printMe(item){
+    // console.log(item);
+    
+}
+
+coding.forEach(printMe)
+
+coding.forEach ((item, index , arr)=> {
+
+})
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "C++",
+        languageFileName : "cpp"
+    },
+    {
+        languageName : "Ruby",
+        languageFileName : "rb"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+    
+})
---
+++ JS_Iterations_5/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple',
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js" , "rb" , "py" , "java" , "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
